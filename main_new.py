0a5173071580ec06ee16c24b43bb8716b3fc34a5
Daniela-Petrea <79249217+Daniela-Petrea@users.noreply.github.com>
2023-01-24 15:49:36 +0200

    Logistic Regression & Random Forest Grid Search

--- main_new.py
+++ main_new.py
@@ -1,16 +1,11 @@
-import string
-
-import pandas as pd
-import numpy as np
-import regex as re
-import nltk
-from nltk import NaiveBayesClassifier
-from nltk.corpus import stopwords
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 from from_file import get_posts_from_file
 from from_file import get_traits_from_file
-
+from sklearn.feature_extraction import DictVectorizer
+import numpy as np
 # nltk.download('punkt')
 
 
@@ -47,48 +42,141 @@ def get_model(posts, traits, position, keys):
     return features
 
 
+def obtain_data(features, features_test):
+    vectorizer = DictVectorizer()
+    X_train, y_train = list(zip(*features))
+    X_train = vectorizer.fit_transform(X_train)
+    X_test, y_test = list(zip(*features_test))
+    X_test = vectorizer.transform(X_test)
+    return X_train, y_train, X_test, y_test
+
 
 traits = ["INTJ", "INTP", "ENTJ", "ENTP", "INFJ", "INFP", "ENFJ", "ENFP", "ISTJ", "ISFJ", "ESTJ", "ESFJ", "ISTP",
           "ISFP", "ESTP", "ESFP"]
 
 list_posts, dict_vocabulary = get_posts_from_file()
 list_traits = get_traits_from_file()
-posts_train, posts_test, traits_train, traits_test = train_test_split(list_posts, list_traits, test_size=0.2, random_state=0)
+posts_train, posts_test, traits_train, traits_test = train_test_split(list_posts, list_traits, test_size=0.2,
+                                                                      random_state=0)
 
 keys = list(dict_vocabulary)
 
 
-# =========== Introversion - Extroversion model ==========
-features = get_model(posts_train, traits_train, 0, keys)
-introversion_extroversion_model = NaiveBayesClassifier.train(features)
-print(nltk.classify.util.accuracy(introversion_extroversion_model, features)*100)
+def grid_search_for_model(grid_parameters,model_name,position):
+    if model_name=="Logistic Regression":
+        features = get_model(posts_train, traits_train, position, keys)
+        features_test = get_model(posts_test, traits_test, position, keys)
+        X_train, y_train, X_test, y_test = obtain_data(features, features_test)
+        gs_log_reg = GridSearchCV(estimator=LogisticRegression(),
+                                param_grid=grid_parameters,
+                                cv=5,
+                                verbose=True)
+        gs_log_reg.fit(X_train, y_train)
+        print(gs_log_reg.best_params_)
+        if position==0:
+            print("Introversion Extroversion Logistic Regression Model Train accuracy percent:",
+                  gs_log_reg.score(X_train, y_train) * 100)
+            print("Introversion Extroversion Logistic Regression Model Test accuracy percent:",
+                  gs_log_reg.score(X_test, y_test) * 100)
+        elif position==1:
+            print("Intuition Sensing Logistic Regression Model Train accuracy percent:",
+                  gs_log_reg.score(X_train, y_train) * 100)
+            print("Intuition Sensing Logistic Regression Model Test accuracy percent:",
+                  gs_log_reg.score(X_test, y_test) * 100)
+        if position==2:
+            print("Thinking Feeling Logistic Regression Model Train accuracy percent:",
+                  gs_log_reg.score(X_train, y_train) * 100)
+            print("Thinking Feeling Logistic Regression Model Test accuracy percent:",
+                  gs_log_reg.score(X_test, y_test) * 100)
+        elif position==3:
+            print("Judging Perceiving Logistic Regression Model Train accuracy percent:",gs_log_reg.score(X_train, y_train) * 100)
+            print("Judging Perceiving Logistic Regression Model Test accuracy percent:", gs_log_reg.score(X_test, y_test) * 100)
+    elif model_name=="Random Forest":
+        features = get_model(posts_train, traits_train, position, keys)
+        features_test = get_model(posts_test, traits_test, position, keys)
+        X_train, y_train, X_test, y_test = obtain_data(features, features_test)
+        gs_rf = GridSearchCV(estimator=RandomForestClassifier(),
+                                param_grid=grid_parameters,
+                                cv=3,
+                                verbose=True)
+        gs_rf.fit(X_train, y_train)
+        print(gs_rf.best_params_)
+        if position==0:
+            print("Introversion Extroversion Random Forest Model Train accuracy percent:",
+                  gs_rf.score(X_train, y_train) * 100)
+            print("Introversion Extroversion Random Forest Model Test accuracy percent:",
+                  gs_rf.score(X_test, y_test) * 100)
+        elif position==1:
+            print("Intuition Sensing Random Forest Model Train accuracy percent:",
+                  gs_rf.score(X_train, y_train) * 100)
+            print("Intuition Sensing Random Forest Model Test accuracy percent:",
+                  gs_rf.score(X_test, y_test) * 100)
+        if position==2:
+            print("Thinking Feeling Random Forest Model Train accuracy percent:",
+                  gs_rf.score(X_train, y_train) * 100)
+            print("Thinking Feeling Random Forest Model Test accuracy percent:",
+                  gs_rf.score(X_test, y_test) * 100)
+        elif position==3:
+            print("Judging Perceiving Random Forest Model Train accuracy percent:",gs_rf.score(X_train, y_train) * 100)
+            print("Judging Perceiving Random Forest Model Test accuracy percent:", gs_rf.score(X_test, y_test) * 100)
 
-features_test = get_model(posts_test, traits_test, 0, keys)
-print(nltk.classify.util.accuracy(introversion_extroversion_model, features_test)*100)
+# # =========== Naive Bayes Classifier ===========
+#
+# # =========== Introversion - Extroversion model ==========
+# features = get_model(posts_train, traits_train, 0, keys)
+# introversion_extroversion_naive_bayes_model = NaiveBayesClassifier.train(features)
+# print("Introversion Extroversion Naive Bayes Model Train accuracy percent:",
+#       nltk.classify.util.accuracy(introversion_extroversion_naive_bayes_model, features) * 100)
+#
+# features_test = get_model(posts_test, traits_test, 0, keys)
+# print("Introversion Extroversion Naive Bayes Model Test accuracy percent:",
+#       nltk.classify.util.accuracy(introversion_extroversion_naive_bayes_model, features_test) * 100)
+#
+# # =========== Intuition - Sensing model ==========
+# features = get_model(posts_train, traits_train, 1, keys)
+# intuition_sensing_naive_bayes_model = NaiveBayesClassifier.train(features)
+# print("Intuition Sensing Naive Bayes Model Train accuracy percent:",
+#       nltk.classify.util.accuracy(intuition_sensing_naive_bayes_model, features) * 100)
+#
+# features_test = get_model(posts_test, traits_test, 1, keys)
+# print("Intuition Sensing Naive Bayes Model Test accuracy percent:",
+#       nltk.classify.util.accuracy(intuition_sensing_naive_bayes_model, features_test) * 100)
+#
+# # =========== Thinking - Feeling model ==========
+# features = get_model(posts_train, traits_train, 2, keys)
+# thinking_feeling_naive_bayes_model = NaiveBayesClassifier.train(features)
+# print("Thinking Feeling Naive Bayes Model Train accuracy percent:",
+#       nltk.classify.util.accuracy(thinking_feeling_naive_bayes_model, features) * 100)
+#
+# features_test = get_model(posts_test, traits_test, 2, keys)
+# print("Thinking Feeling Naive Bayes Model Test accuracy percent:",
+#       nltk.classify.util.accuracy(thinking_feeling_naive_bayes_model, features_test) * 100)
+#
+# # =========== Judging - Perceiving model ==========
+# features = get_model(posts_train, traits_train, 3, keys)
+# judging_perceiving_naive_bayes_model = NaiveBayesClassifier.train(features)
+# print("Judging Perceiving Naive Bayes Model Train accuracy percent:",
+#       nltk.classify.util.accuracy(judging_perceiving_naive_bayes_model, features) * 100)
+#
+# features_test = get_model(posts_test, traits_test, 3, keys)
+# print("Judging Perceiving Naive Bayes Model Test accuracy percent:",
+#       nltk.classify.util.accuracy(judging_perceiving_naive_bayes_model, features_test) * 100)
 
+print("======================================================================")
 
-# =========== Intuition - Sensing model ==========
-features = get_model(posts_train, traits_train, 1, keys)
-intuition_sensing_model = NaiveBayesClassifier.train(features)
-print(nltk.classify.util.accuracy(intuition_sensing_model, features)*100)
+log_reg_grid = {"C": np.logspace(-4, 4, 30),
+                    "solver": ["liblinear"]}
+grid_search_for_model(log_reg_grid,"Logistic Regression",0)
+grid_search_for_model(log_reg_grid,"Logistic Regression",1)
+grid_search_for_model(log_reg_grid,"Logistic Regression",2)
+grid_search_for_model(log_reg_grid,"Logistic Regression",3)
+rf_grid = {#"n_estimators": np.arange(100, 1000, 100),
+           "max_depth": [None, 3, 5, 10],
+           "min_samples_split": np.arange(2, 20, 3),
+           #"min_samples_leaf": np.arange(1, 20, 3)
+           }
+grid_search_for_model(rf_grid,"Random Forest",0)
+grid_search_for_model(rf_grid,"Random Forest",1)
+grid_search_for_model(rf_grid,"Random Forest",2)
+grid_search_for_model(rf_grid,"Random Forest",3)
 
-features_test = get_model(posts_test, traits_test, 1, keys)
-print(nltk.classify.util.accuracy(intuition_sensing_model, features_test)*100)
-
-
-# =========== Thinking - Feeling model ==========
-features = get_model(posts_train, traits_train, 2, keys)
-thinking_feeling_model = NaiveBayesClassifier.train(features)
-print(nltk.classify.util.accuracy(thinking_feeling_model, features)*100)
-
-features_test = get_model(posts_test, traits_test, 2, keys)
-print(nltk.classify.util.accuracy(thinking_feeling_model, features_test)*100)
-
-
-# =========== Judging - Perceiving model ==========
-features = get_model(posts_train, traits_train, 3, keys)
-judging_perceiving_model = NaiveBayesClassifier.train(features)
-print(nltk.classify.util.accuracy(judging_perceiving_model, features)*100)
-
-features_test = get_model(posts_test, traits_test, 3, keys)
-print(nltk.classify.util.accuracy(judging_perceiving_model, features_test)*100)
