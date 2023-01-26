6dd93417bfa22d8906d2d301cc1e6677d58561f1
Madalina Carausu <madalinacarausu@gmail.com>
2023-01-26 11:18:20 +0200

    Add gui

---
+++ backend.py
@@ -0,0 +1,34 @@
+def svm(text):
+    response = "SVM"
+    # Insert code here
+    return response
+
+
+def decision_tree(text):
+    response = "LSTM"
+    # Insert code here
+    return response
+
+
+def random_forest(text):
+    response = "Random Forest"
+    # Insert code here
+    return response
+
+
+def logistic_regression(text):
+    response = "Logistic Regression"
+    # Insert code here
+    return response
+
+
+def naive_bayes(text):
+    response = "Naive Bayes"
+    # Insert code here
+    return response
+
+
+def knn(text):
+    response = "KNN"
+    # Insert code here
+    return response
