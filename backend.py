d0f92094ce5c7c93c4f65298898c006f8747b46d
HAIURA Andreea-Isabela <andreea_haiura@yahoo.com>
2023-01-26 12:09:27 +0200

    integration

--- backend.py
+++ backend.py
@@ -1,34 +1,37 @@
+from load_classifiers import modify_data
+from load_classifiers import grid_search_for_model
+
+
 def svm(text):
-    response = "SVM"
-    # Insert code here
+    response = grid_search_for_model("SVM", modify_data(text))
     return response
 
 
 def decision_tree(text):
-    response = "LSTM"
-    # Insert code here
+    response = grid_search_for_model("Decision Tree", modify_data(text))
     return response
 
 
 def random_forest(text):
-    response = "Random Forest"
-    # Insert code here
+    response = grid_search_for_model("Random Forest", modify_data(text))
     return response
 
 
 def logistic_regression(text):
-    response = "Logistic Regression"
-    # Insert code here
+    response = grid_search_for_model("Logistic Regression", modify_data(text))
     return response
 
 
 def naive_bayes(text):
-    response = "Naive Bayes"
-    # Insert code here
+    response = grid_search_for_model("Naive Bayes", modify_data(text))
     return response
 
 
 def knn(text):
-    response = "KNN"
-    # Insert code here
+    response = grid_search_for_model("KNN", modify_data(text))
+    return response
+
+
+def ensemble(text):
+    response = grid_search_for_model("Ensemble", modify_data(text))
     return response
